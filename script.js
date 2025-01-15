729ef0bde2553421e5f3b40d078803262388d277
SJ99677 <sjjohnson2003@outlook.com>
2025-01-14 23:55:29 -0500

    Fix bugs, Update script.js
    Fixed bugs in the display and adjusted coloring for buttons when hovering and clicking. Also added JS for calculator functionality.

---
+++ script.js
@@ -0,0 +1,20 @@
+//Functionality
+
+const display = document.getElementById("display");
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearScreen(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+    
+}
